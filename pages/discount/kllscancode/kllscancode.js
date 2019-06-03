0becfb5d35ede4cdda4a7b0c0b06a46f0ce1b275
郝艳萍 <2860435000@qq.com>
2019-06-03 09:41:39 +0800

    kllscacode.js文件找回

--- pages/discount/kllscancode/kllscancode.js
+++ pages/discount/kllscancode/kllscancode.js
@@ -8,9 +8,9 @@ var userinfo = f.getCache('userinfo');
 console.log(userinfo)
 var moneycount = ''
 var calorienum = ''
-var actualnum=''
-var merchid=31
-var itemid=''
+var actualnum = ''
+var merchid = 31
+var itemid = ''
 Page({
 
     /**
@@ -18,11 +18,11 @@ Page({
      */
     data: {
         globalimg: i.globalData.appimg,
-        moneynum:'',
-        caloriecount:'',
-        actualcount:'',
-        fulllist:[],
-        usercredit:''
+        moneynum: '',
+        caloriecount: '',
+        actualcount: '',
+        fulllist: [],
+        usercredit: ''
     },
 
     /**
@@ -32,21 +32,21 @@ Page({
         // 修改卡路里列表页的数据
         console.log(merchid)
         var b = decodeURIComponent(t.scene);
-            var i = s.str2Obj(b);
-            t.id = i.id;
-            console.log(t)
-            console.log(b)
-            var a=this
+        var i = s.str2Obj(b);
+        t.id = i.id;
+        console.log(t)
+        console.log(b)
+        var a = this
         wx.request({
             url: 'https://paokucoin.com/app/index.php?i=1&c=entry&m=ewei_shopv2&do=mobile&r=app.payment.index.getset',
             data: {
-                cate:1,
-                merchid:merchid
+                cate: 1,
+                merchid: merchid
             },
             success: function (e) {
                 console.log(e.data)
                 a.setData({
-                   fulllist:e.data.result.list
+                    fulllist: e.data.result.list
                 })
             }
         })
@@ -60,21 +60,21 @@ Page({
     onReady: function () {
 
     },
-//   获取用户输入的金额
-  moneyInput: function (e) {
-      moneycount = e.detail.value
-      calorienum = Math.floor((e.detail.value / 50)) * 5
-      actualnum = e.detail.value - Math.floor((e.detail.value / 50)) * 5
-    this.setData({
-      moneynum: e.detail.value,
-        caloriecount: Math.floor((e.detail.value / 50)) * 5,
-        actualcount: e.detail.value - Math.floor((e.detail.value / 50)) * 5
-    })
-      console.log(actualnum)
-  },
-//   立即买单
-    paymentbtn:function(){
-        console.log(moneycount )
+    //   获取用户输入的金额
+    moneyInput: function (e) {
+        moneycount = e.detail.value
+        calorienum = Math.floor((e.detail.value / 50)) * 5
+        actualnum = e.detail.value - Math.floor((e.detail.value / 50)) * 5
+        this.setData({
+            moneynum: e.detail.value,
+            caloriecount: Math.floor((e.detail.value / 50)) * 5,
+            actualcount: e.detail.value - Math.floor((e.detail.value / 50)) * 5
+        })
+        console.log(actualnum)
+    },
+    //   立即买单
+    paymentbtn: function () {
+        console.log(moneycount)
         console.log(calorienum)
     },
 
@@ -82,16 +82,16 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-      var t=this
+        var t = this
         wx.request({
             url: 'https://paokucoin.com/app/index.php?i=1&c=entry&m=ewei_shopv2&do=mobile&r=app.payment.index.getCredit',
             data: {
-                openid:userinfo.openid
+                openid: userinfo.openid
             },
             success: function (e) {
                 console.log(e.data)
-               t.setData({
-                   usercredit:e.data.result.credit1
+                t.setData({
+                    usercredit: e.data.result.credit1
                 })
             }
         })
