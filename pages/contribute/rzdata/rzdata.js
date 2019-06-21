54c7b2ed7e1aa97ff6e25292f13a4396f4c40972
郝艳萍 <2860435000@qq.com>
2019-06-21 18:00:46 +0800

    折扣付页面及接口，漂浮币定位

---
+++ pages/contribute/rzdata/rzdata.js
@@ -0,0 +1,124 @@
+// pages/contribute/rzdata/rzdata.js
+var a, e, i = getApp(),
+    s = i.requirejs("core");
+//   当前登录人的openid
+var f = getApp();
+var userinfo = f.getCache('userinfo');
+var userwechat=''
+var message=''
+// var usermobile=''
+// var userweixin=''
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        globalimg: i.globalData.appimg,
+        maskDis:'none',
+        wechatnumber:'',
+        usermobile:'',
+        userweixin:''
+
+    },
+    gobindbtn:function(){
+        wx.navigateTo({
+            url: '/pages/member/bind/index',
+        })
+    },
+    wechatbtn:function(){
+        this.setData({
+            maskDis: 'block'
+        })
+    },
+    inputChange: function (event) {
+        userwechat = event.detail.value
+        console.log(userwechat)
+    },
+    confirmbtn:function(){
+            var a=this
+            a.setData({
+                maskDis: 'none',
+                userweixin: userwechat
+            })
+        s.get("myown/devote/wx", {
+            openid: userinfo.openid,
+            weixin: userwechat
+        }, function (e) {
+            console.log(e)
+            if(e.message=='成功'){
+                message=e.message
+                wx.showToast({
+                    title: message,
+                    duration:2000
+                })
+            }
+        })
+    },
+    backBtn:function(){
+        wx.navigateTo({
+            url: '/pages/contribute/contribute/contribute',
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        // 修改资料传过来的参数
+        console.log(options)
+        var t=this
+        t.setData({
+            usermobile:options.mobile,
+            userweixin:options.weixin
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
---
+++ pages/contribute/withdraw/withdraw.js
@@ -0,0 +1,124 @@
+// pages/discount/zkbrechange/zkbrechange.js
+var a, e, i = getApp(),
+    s = i.requirejs("core");
+//   当前登录人的openid
+var f = getApp();
+var useropenid = ''
+var iptvalue = ''
+var creditnum = ''
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        globalimg: i.globalData.appimg,
+        hintDis: 'none',
+        usercredit: ''
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var userinfo = f.getCache('userinfo');
+        useropenid = userinfo.openid
+        var t = this
+        s.get("payment/index/getCredit", {
+            openid: useropenid
+        }, function (e) {
+            console.log(e)
+            creditnum = Number(e.result.credit1)
+            t.setData({
+                usercredit: e.result.credit1
+            })
+        })
+    },
+    // 监听输入
+    watchPassWord: function (event) {
+        var a = this
+        console.log(event.detail.value);
+        iptvalue = event.detail.value
+        if (event.detail.value > creditnum) {
+            a.setData({
+                hintDis: 'block'
+            })
+        } else {
+            a.setData({
+                hintDis: 'none'
+            })
+        }
+    },
+    rechargebtn: function () {
+        console.log(creditnum)
+        console.log(iptvalue)
+        if (iptvalue <= creditnum) {
+            s.get("payment/index/change", {
+                money: iptvalue,
+                openid: useropenid
+            }, function (eve) {
+                console.log(eve)
+                if (eve.status == 1) {
+                    wx.navigateTo({
+                        url: '/pages/discount/resuccess/resuccess',
+                    })
+                }
+            })
+
+        } else {
+            wx.showModal({
+                title: '提示',
+                content: '您的贡献值不足，请重新输入',
+            })
+        }
+
+    },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
