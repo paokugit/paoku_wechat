159c8c920f90cb35efb0ab7b96f07efd7180ccfe
xph <2595222190@qq.com>
2019-07-19 18:53:28 +0800

    加速宝

---
+++ pages/huiyuan/expedite/expedite.js
@@ -0,0 +1,172 @@
+// pages/huiyuan/expedite/expedite.js
+var t = getApp(),
+  a = t.requirejs("core");
+var f = getApp();  
+
+var speedId = "";//加速宝id
+var useropenid = "";//用户openid
+
+var isSelect = "";//点击显示的判断条件
+
+var order_id = "";//订单ID
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    globalimg: t.globalData.appimg,
+    // 组件所需的参数
+    nvabarData: {
+      showCapsule: 1,
+      title: '加速宝',
+      height: t.globalData.height * 2 + 20,
+    },
+    list:[],//列表数据
+    isShow:false,//弹出层判断
+    payDay:'',//加速天数内容
+    tit_one:'',//标题提示内容
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var userinfo = f.getCache('userinfo');
+    useropenid = userinfo.openid
+    var b = this;
+    a.get("myown/acceleration/index", {}, function (e) {
+      b.setData({
+        list:e.message,
+      })
+    });
+  },
+
+  
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  // 列表点击事件
+  list_btn:function(e){
+    speedId = e.currentTarget.dataset.id;
+    
+    this.setData({
+      isSelect: speedId,
+    })
+  },
+
+  // 立即支付点击事件
+  pay_btn:function(e){
+    var t = this;
+    a.get("myown.acceleration.order", {
+      openid: useropenid,
+      id: speedId
+    }, function (e) {
+      console.log(e);
+      order_id = e.message.order_id;
+
+      // 支付成功判断
+      if (e.error == '0') {
+        wx.requestPayment({
+          timeStamp: e.message.wx.timeStamp, 
+          nonceStr: e.message.wx.nonceStr,
+          package: e.message.wx.package,
+          signType: 'MD5',
+          paySign: e.message.wx.paySign,
+          success(res) {
+            console.log(res);
+
+            // 支付成功后的接口请求
+            a.get("myown.acceleration.wx_back",{
+              order_id: order_id
+            },function(e){
+              console.log(e);
+              if(e.error == "0"){
+                wx.navigateTo({
+                  url: '../pay_succeed/pay_succeed?order_id='+order_id,
+                })
+              }
+            })
+
+          },
+          fail(res) {
+            console.log(res);
+          }
+        })
+      };  
+
+      // 支付失败判断
+      if(e.error == '1'){
+        t.setData({
+          isShow: true,
+          tit_one:'请选择加速天数'
+        }) 
+      };  
+
+      // 加速期内判断
+      if(e.error == '2'){
+        t.setData({
+          isShow:true,
+          payDay: e.message.day +'天后结束',
+          tit_one:'已经在加速期，请结束后在买哦'
+        })
+      };
+
+    });
+  },
+
+  // 弹出窗隐藏点击
+  know_btn: function () {
+    this.setData({
+      isShow: false
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+ 
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/huiyuan/membercenter/membercenter.js
+++ pages/huiyuan/membercenter/membercenter.js
@@ -1,238 +1,264 @@
-var a, e, i = getApp(), s = i.requirejs("core"), n = i.requirejs("wxParse/wxParse"),
-    r = i.requirejs("biz/diyform"), d = i.requirejs("biz/goodspicker"), c = (i.requirejs("foxui"),
-        i.requirejs("jquery"));
-        var levelid=''
+var a, e, i = getApp(),
+  s = i.requirejs("core"),
+  n = i.requirejs("wxParse/wxParse"),
+  r = i.requirejs("biz/diyform"),
+  d = i.requirejs("biz/goodspicker"),
+  c = (i.requirejs("foxui"),
+    i.requirejs("jquery"));
+var levelid = ''
 //   当前登录人的openid
 var f = getApp();
 var userinfo = f.getCache('userinfo');
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        globalimg: i.globalData.appimg,
-        // 组件所需的参数
-        nvabarData: {
-            showCapsule: 0, //是否显示左上角图标   1表示显示    0表示不显示
-            title: '达人中心', //导航栏 中间的标题
-            // 此页面 页面内容距最顶部的距离
-            height: i.globalData.height * 2 + 20,
-        },
-        color:'green',
-        nickName: '',
-        avatarUrl: '',
-        loaded: !1,
-        loading: !0,
-        speeddone:'none',
-        speeding:'block',
-        dateDis:'block',
-        avatar:'',
-        levelid:'',
-        endtime:'',
-        levelname:'',
-        leveltime:'',
-        addspead:"",
-        surplus_day:"",
-        give_day:"",
-        accelerate_day:"",
-        levelinfo:'',
-        imglist:[
-            {
-                imgurl: '/icon1/sport-n.png',
-                hoverurl:'/icon1/sport-s.png'
-            },
-            {
-                imgurl: '/icon1/home-n.png',
-                hoverurl: '/icon1/home-s.png'
-            },
-            {
-                imgurl: '/icon1/shop-n.png',
-                hoverurl: '/icon1/shop-s.png'
-            },
-            {
-                imgurl: '/icon1/huiyuan-n.png',
-                hoverurl: '/icon1/huiyuan-s.png'
-            },
-            {
-                imgurl: '/icon1/user-n.png',
-                hoverurl: '/icon1/user-s.png'
-            },
-        ],
-        imgHoverIndex: '',
-        arr01: [
-            {
-                img: 'https://paokucoin.com/img/backgroup/qiandao.png',
-            },
-            // {
-            //     img: 'https://paokucoin.com/img/backgroup/zhuce.png',
-            // },
-            // {
-            //     img: 'https://paokucoin.com/img/backgroup/ziliao.png',
-            // },
-            {
-                img: 'https://paokucoin.com/img/backgroup/sharehaibao.png',
-            },
-            ]
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    globalimg: i.globalData.appimg,
+    // 组件所需的参数
+    nvabarData: {
+      showCapsule: 0, //是否显示左上角图标   1表示显示    0表示不显示
+      title: '达人中心', //导航栏 中间的标题
+      // 此页面 页面内容距最顶部的距离
+      height: i.globalData.height * 2 + 20,
     },
-    shengji:function(){
-        wx.navigateTo({
-            url: '../hygrade/hygrade?hyid=' + levelid,
+    color: 'green',
+    nickName: '',
+    avatarUrl: '',
+    loaded: !1,
+    loading: !0,
+    speeddone: 'none',
+    speeding: 'block',
+    dateDis: 'block',
+    avatar: '',
+    levelid: '',
+    endtime: '',
+    levelname: '',
+    leveltime: '',
+    addspead: "",
+    surplus_day: "",
+    give_day: "",
+    accelerate_day: "",
+    levelinfo: '',
+
+    member_css:"none",//赠送加速
+    precious_css:"none",//加速宝生效
+
+    imglist: [{
+        imgurl: '/icon1/sport-n.png',
+        hoverurl: '/icon1/sport-s.png'
+      },
+      {
+        imgurl: '/icon1/home-n.png',
+        hoverurl: '/icon1/home-s.png'
+      },
+      {
+        imgurl: '/icon1/shop-n.png',
+        hoverurl: '/icon1/shop-s.png'
+      },
+      {
+        imgurl: '/icon1/huiyuan-n.png',
+        hoverurl: '/icon1/huiyuan-s.png'
+      },
+      {
+        imgurl: '/icon1/user-n.png',
+        hoverurl: '/icon1/user-s.png'
+      },
+    ],
+    imgHoverIndex: '',
+    arr01: [{
+        img: 'https://paokucoin.com/img/backgroup/qiandao.png',
+      },
+      // {
+      //     img: 'https://paokucoin.com/img/backgroup/zhuce.png',
+      // },
+      // {
+      //     img: 'https://paokucoin.com/img/backgroup/ziliao.png',
+      // },
+      {
+        img: 'https://paokucoin.com/img/backgroup/sharehaibao.png',
+      },
+    ]
+  },
+  shengji: function() {
+    wx.navigateTo({
+      url: '../hygrade/hygrade?hyid=' + levelid,
+    })
+  },
+
+  //xph:跳转加速宝也面
+  speed_up: function() {
+    wx.navigateTo({
+      url: '../expedite/expedite'
+    })
+  },
+
+
+  // 邀请好友
+  invite: function() {
+    wx.navigateTo({
+      url: '../../commission/down/index',
+    })
+  },
+  hbshare: function() {
+    wx.navigateTo({
+      url: '/pages/sportdiary/sportdiary/sportdiary',
+    })
+  },
+  zlBtn: function() {
+    wx.showToast({
+      title: '暂未开放',
+      duration: 2000
+    })
+  },
+  sportBtn: function(e) {
+    console.log(e)
+    // this.setData({
+    //     imgHoverIndex: e.currentTarget.dataset.index
+    // })
+    wx.switchTab({
+      url: '../../index/index',
+    })
+  },
+  shopBtn: function() {
+    wx.switchTab({
+      url: '../../index/huodong',
+    })
+  },
+  carBtn: function() {
+    wx.switchTab({
+      url: '/pages/discount/discount/discount',
+    })
+  },
+  userBtn: function() {
+    wx.switchTab({
+      url: '../../member/index/index',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var i = this
+    // let open = wx.getStorageSync('openid')
+    s.get("member", {
+      openid: userinfo.openid
+    }, function(e) {
+      console.log(e)
+      levelid = e.levelid
+      i.setData({
+        nickname: e.nickname,
+        avatar: e.avatar,
+        levelid: e.levelid,
+        endtime: e.endtime,
+        levelname: e.levelname,
+        leveltime: e.leveltime,
+        levelinfo: e.levelinfo
+      })
+    })
+    let myUserInfo = wx.getStorageSync('myUserInfo');
+    this.setData({
+      nickName: myUserInfo.nickName,
+      avatarUrl: myUserInfo.avatarUrl
+    })
+    var ttt = this
+    s.get("myown/index/accelerate", {
+      openid: userinfo.openid
+    }, function(eve) {
+      console.log(eve)
+      if (eve.message.surplus_day == 0) {
+        ttt.setData({
+          speeding: 'none',
+          speeddone: 'block'
         })
-    },
-// 邀请好友
-    invite:function(){
-        wx.navigateTo({
-        url: '../../commission/down/index',
-})
-    }, 
-    hbshare:function(){
-        wx.navigateTo({
-            url: '/pages/sportdiary/sportdiary/sportdiary',
+      } else {
+        ttt.setData({
+          speeding: 'block',
+          speeddone: 'none' 
+        }) 
+      }
+      if (eve.message.type == 0){
+        ttt.setData({
+          member_css: "block",
+          precious_css: "none"
         })
-    },
-    zlBtn:function(){
-        wx.showToast({
-            title: '暂未开放',
-            duration:2000
+      } else if (eve.message.type == 1){
+        ttt.setData({
+          member_css: "none",
+          precious_css: "block"
         })
-    },
-    sportBtn:function(e){
-        console.log(e)
-        // this.setData({
-        //     imgHoverIndex: e.currentTarget.dataset.index
-        // })
-        wx.switchTab({
-            url: '../../index/index',
-        })
-    },
-    shopBtn: function () {
-        wx.switchTab({
-            url: '../../index/huodong',
-        })
-    },
-    carBtn: function () {
-        wx.switchTab({
-          url: '/pages/discount/discount/discount',
-        })
-    },
-    userBtn: function () {
-        wx.switchTab({
-            url: '../../member/index/index',
-        })
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        var i=this
-        // let open = wx.getStorageSync('openid')
-        s.get("member", {
-            openid: userinfo.openid
-        }, function (e){
-            console.log(e)
-            levelid = e.levelid
-        i.setData({
-            nickname: e.nickname,
-            avatar: e.avatar,
-            levelid: e.levelid,
-            endtime: e.endtime,
-            levelname: e.levelname,
-            leveltime: e.leveltime,
-            levelinfo: e.levelinfo
-        })
-        })
-        let myUserInfo = wx.getStorageSync('myUserInfo');
-        this.setData({
-            nickName: myUserInfo.nickName,
-            avatarUrl: myUserInfo.avatarUrl
-        })
-        var ttt=this
-        s.get("myown/index/accelerate", {
-            openid: userinfo.openid
-        },function(eve){
-            console.log(eve)
-            if (eve.message.surplus_day == 0) {
-               ttt.setData({
-                    speeding: 'none',
-                    speeddone: 'block'
-                })
-            } else {
-               ttt.setData({
-                    speeding: 'block',
-                    speeddone: 'none'
-                })
-            }
-            ttt.setData({
-                credit: eve.message.credit,
-                surplus_day: eve.message.surplus_day,
-                give_day: eve.message.give_day,
-                accelerate_day: eve.message.accelerate_day,
-                addspead: (eve.message.accelerate_day) / (eve.message.accelerate_day + eve.message.surplus_day) * 100
-            })
-        } )
-       
-    },
+      }
+      ttt.setData({
+        credit: eve.message.credit,
+        surplus_day: eve.message.surplus_day,
+        give_day: eve.message.give_day,
+        accelerate_day: eve.message.accelerate_day,
+        addspead: (eve.message.accelerate_day) / (eve.message.accelerate_day + eve.message.surplus_day) * 100
+      })
+    })
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+  },
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
+  },
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function (res) {
-        // return s.onShareAppMessage();
-        var that = this;
-        return {
-            title: '原来微信步数可以当钱用，快来和我一起薅羊毛',
-            path: '/pages/index/index?id=' + that.data.scratchId,
-            success: function (res) {
-                // 转发成功
+  },
 
-                that.shareClick();
-            },
-            fail: function (res) {
-                // 转发失败
-            }
-        }
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function(res) {
+    // return s.onShareAppMessage();
+    var that = this;
+    return {
+      title: '原来微信步数可以当钱用，快来和我一起薅羊毛',
+      path: '/pages/index/index?id=' + that.data.scratchId,
+      success: function(res) {
+        // 转发成功
+
+        that.shareClick();
+      },
+      fail: function(res) {
+        // 转发失败
+      }
     }
+  }
 })
