3a2f310e68f9743c5799c662a376ac8250a8d7d6
郝艳萍 <2860435000@qq.com>
2019-08-07 09:28:41 +0800

    贡献机

--- pages/discount/discount/discount.js
+++ pages/discount/discount/discount.js
@@ -2,17 +2,17 @@ var a, e, i = getApp(),
     s = i.requirejs("core");
 //   当前登录人的openid
 var f = getApp();
-var merchid=''
-var useropenid=''
+var merchid = ''
+var useropenid = ''
 var timestamp = ''
 var noncestr = ''
 var pack = ''
 var signtype = ''
 var paysign = ''
-var own=''
-var agentlevel=""
-var conbind=''
-var creditnum=""
+var own = ''
+var agentlevel = ""
+var conbind = ''
+var creditnum = ""
 Page({
 
     /**
@@ -22,36 +22,42 @@ Page({
         globalimg: i.globalData.appimg,
         // 组件所需的参数
         nvabarData: {
-            showCapsule: 0, 
-            title: '折扣付', 
+            showCapsule: 0,
+            title: '折扣付',
             height: i.globalData.height * 2 + 30,
         },
         credit: '',
         conbind: '',
         credit4: '',
-        tixian:'',
+        tixian: '',
         len: '',
         animation: [],
+        gxzDis: 'none',
+        opengxz: 'block',
+        gxzlist: [],
+        valid: "",
+        no_valid: "",
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        var t=this
+        var t = this
         var userinfo = f.getCache('userinfo');
         console.log(userinfo)
         useropenid = userinfo.openid
         own = userinfo.is_own
         agentlevel = userinfo.agentlevel
-        if(userinfo.merchInfo==undefined || userinfo.merchInfo==false){
-            merchid=undefined
-        }else{
-            merchid=userinfo.merchInfo.id
+        if (userinfo.merchInfo == undefined || userinfo.merchInfo == false) {
+            merchid = undefined
+        } else {
+            merchid = userinfo.merchInfo.id
         }
-        
+
     },
-    withdrawbtn: function () {
+    // 提现
+    withdrawbtn: function() {
         console.log(conbind)
         if (conbind == 0) {
             wx.showModal({
@@ -64,12 +70,12 @@ Page({
             })
         }
     },
-    transferbtn: function () {
+    transferbtn: function() {
         wx.navigateTo({
             url: '/pages/discount/transfer/transfer',
         })
     },
-    rechargebtn: function () {
+    rechargebtn: function() {
         wx.navigateTo({
             url: '/pages/discount/zkbrechange/zkbrechange',
         })
@@ -92,16 +98,16 @@ Page({
         })
     },
     codebtn: function() {
-        if (merchid==undefined) {
+        if (merchid == undefined) {
             console.log(agentlevel)
-            if (agentlevel == 0 || agentlevel==1){
+            if (agentlevel == 0 || agentlevel == 1) {
                 wx.showModal({
                     title: '提示',
                     content: '成为星选达人或店主即可开通',
-                    showCancel: true, 
-                    cancelText: "知道了", 
-                    confirmText: "去开通", 
-                    success: function (res) {
+                    showCancel: true,
+                    cancelText: "知道了",
+                    confirmText: "去开通",
+                    success: function(res) {
                         if (res.cancel) {
                             console.log("取消")
                         } else if (res.confirm) {
@@ -113,21 +119,21 @@ Page({
                         }
                     }
                 })
-            } else if (agentlevel==2){
+            } else if (agentlevel == 2) {
                 wx.navigateTo({
                     url: '/pages/discount/merchcode/merchcode',
                 })
-            } else if (agentlevel == 5){
+            } else if (agentlevel == 5) {
                 wx.navigateTo({
                     url: '/pages/discount/merchcode/merchcode',
                 })
             }
-            
-        }else{
+
+        } else {
             wx.navigateTo({
                 url: '/pages/discount/merchcode/merchcode',
             })
-         }
+        }
 
     },
     /**
@@ -144,7 +150,7 @@ Page({
         var a = this
         s.get("payment/index/getCredit", {
             openid: useropenid
-        }, function (e) {
+        }, function(e) {
             console.log(e)
             a.setData({
                 credit: e.result.credit3
@@ -152,45 +158,112 @@ Page({
         })
         s.get("myown/devote/msg", {
             openid: useropenid,
-        }, function (e) {
+        }, function(e) {
             console.log(e)
             if (e.error == 0) {
                 conbind = e.message.bind
+                if (conbind == 1) {
+                    a.setData({
+                        gxzDis: 'block',
+                        opengxz: 'none'
+                    })
+                } else if (conbind == 0) {
+                    a.setData({
+                        opengxz: 'block',
+                        gxzDis: 'none'
+                    })
+                }
                 creditnum = e.message.credit4
-                // a.show_num(creditnum)
+                a.show_num(Number(e.message.credit4))
+
                 a.setData({
-                    conbind: e.message.bind,
-                    credit4: e.message.credit4,
+                    // conbind: e.message.bind,
+                    // credit4: e.message.credit4,
                     tixian: e.message.tixian
+                })
+            }
+        });
+        // 贡献机
+        s.get("payment/myown/devote", {
+            openid: useropenid
+        }, function(e) {
+            console.log(e)
+            if (e.status == 1) {
+                a.setData({
+                    valid: e.result.valid,
+                    no_valid: e.result.no_valid,
+                    gxzlist: e.result.list
                 })
             }
         })
     },
-    // show_num(n) {
-    //     var len = String(n).length;
-    //     this.setData({
-    //         len: len,
-    //     })
-    //     var char = String(n).split("")
-    //     // h存储数字块高度
-    //     var h = ''
-    //     let self = this
-    //     // 创造节点选择器
-    //     wx.createSelectorQuery().select('.unit-num').boundingClientRect(function (rect) {
-    //         h = rect.height
-    //         var animate = []
-    //         for (var i = 0; i < len; i++) {
-    //             animate[i] = wx.createAnimation()
-    //             animate[i].top(-parseInt(h) * char[i]).step({
-    //                 duration: 1500
-    //             })
-    //             var deletedtodo = 'animation[' + i + ']';
-    //             self.setData({
-    //                 [deletedtodo]: animate[i].export()
-    //             })
-    //         }
-    //     }).exec()
-    // },
+    getdevotebtn: function(event) {
+        console.log(event)
+        s.get("payment/myown/get_devote", {
+            ids: event.currentTarget.dataset.id,
+            openid: useropenid
+        }, function(e) {
+            console.log(e)
+            if (e.status == 1) {
+                wx.showModal({
+                    title: '提示',
+                    content: e.result.message,
+                    success: function(res) {
+                        if (res.cancel) {
+                            // 取消
+                        } else {
+                            // 确定
+                            wx.navigateTo({
+                                url: '/pages/contribute/record/record',
+                            })
+                        }
+                    }
+                })
+            } else {
+                wx.showModal({
+                    title: '提示',
+                    content: e.result.message,
+                })
+            }
+        })
+    },
+    getalready: function() {
+        wx.showModal({
+            title: '提示',
+            content: '今日贡献值已领取',
+        })
+    },
+    notget: function() {
+        wx.showModal({
+            title: '提示',
+            content: '该贡献值未开启',
+        })
+    },
+    show_num(n) {
+        var len = String(n).length;
+        this.setData({
+            len: len,
+        })
+        var char = String(n).split("")
+        // h存储数字块高度
+        var h = ''
+        let self = this
+        // 创造节点选择器
+        wx.createSelectorQuery().select('.unit-num').boundingClientRect(function(rect) {
+            h = rect.height
+            var animate = []
+            for (var i = 0; i < len; i++) {
+                animate[i] = wx.createAnimation()
+                animate[i].top(-parseInt(h) * (Number(char[i]) + 1)).step({
+                    duration: 1500
+                })
+                var deletedtodo = 'animation[' + i + ']';
+                self.setData({
+                    [deletedtodo]: animate[i].export()
+                })
+            }
+        }).exec()
+    },
 
     /**
      * 生命周期函数--监听页面隐藏
@@ -212,7 +285,7 @@ Page({
     onPullDownRefresh: function() {
         wx.showToast({
             icon: 'loading',
-            title:'加载中'
+            title: '加载中'
         });
         wx.stopPullDownRefresh();
     },
