ab9a361b4e7c9f294577ad27e8263d5e05807e0b
郝艳萍 <2860435000@qq.com>
2019-11-04 18:35:29 +0800

    退货列表

--- pages/order/exchange/list.js
+++ pages/order/exchange/list.js
@@ -11,13 +11,47 @@ Page({
     showIcon: true,
     gloheight: o.globalData.gloheight,
     globalimg: o.globalData.appimg,
-    carts: [], // 购物车列表
+    batchIds: '', //选中的ids
 
+    carts: [
+
+      {
+        id: "1",
+        title: '新鲜芹菜 半斤',
+        image: 'https://www.paokucoin.com/attachment/images/1/2019/09/jJLLGzjl3HG01GR0gzGLLznRB6H7hF.jpg',
+        num: 1,
+        price: 10,
+        selected: false
+      },
+
+      {
+        id: "2",
+        title: '素米 500g',
+        image: 'https://www.paokucoin.com/attachment/images/1/2019/09/E3X0rX7jXaXZ3aXKQmnaRlzzXAN0m3.jpg',
+        num: 1,
+        price: 20,
+        selected: false
+      }
+    ],
+    select_all: false,
+    listData: [{
+        code: "1",
+        text: "测试1"
+      },
+      {
+        code: "2",
+        text: "测试2"
+      },
+      {
+        code: "3",
+        text: "测试3"
+      }
+    ],
     hasList: false, // 列表是否有数据
 
     totalPrice: 0, // 总价，初始为0
 
-    selectAllStatus: false // 全选状态，默认全选
+    selectAllStatus: false // 全选状态，默认非全选
   },
 
   /**
@@ -40,92 +74,46 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
     this.setData({
-
       hasList: true, // 既然有数据了，那设为true吧
-
-      carts: [
-
-        {
-          id: 1,
-          title: '新鲜芹菜 半斤',
-          image: 'https://www.paokucoin.com/attachment/images/1/2019/09/jJLLGzjl3HG01GR0gzGLLznRB6H7hF.jpg',
-          num: 1,
-          price: 10,
-          selected: false
-        },
-
-        {
-          id: 2,
-          title: '素米 500g',
-          image: 'https://www.paokucoin.com/attachment/images/1/2019/09/E3X0rX7jXaXZ3aXKQmnaRlzzXAN0m3.jpg',
-          num: 1,
-          price: 20,
-          selected: false
-        }
-
-      ]
-
     });
-
   },
-  getTotalPrice() {
-
-    let carts = this.data.carts; // 获取购物车列表
-
-    let total = 0;
-
-    for (let i = 0; i < carts.length; i++) { // 循环列表得到每个数据
-
-      if (carts[i].selected) { // 判断选中才会计算价格
-
-        total += carts[i].num * carts[i].price; // 所有价格加起来
-
+  //全选与反全选
+  selectAll: function(e) {
+    console.log(e)
+    var that = this;
+    let selectAllStatus = that.data.selectAllStatus; // 是否全选状态
+    selectAllStatus = !selectAllStatus;
+    var arr = []; //存放选中id的数组
+    console.log(selectAllStatus)
+    for (let i = 0; i < that.data.carts.length; i++) {
+      that.data.carts[i].selected = selectAllStatus; 
+      if (selectAllStatus == true) {
+        // 全选获取选中的值
+        arr = arr.concat(that.data.carts[i].id.split(','));
       }
-
     }
-
-    this.setData({ // 最后赋值到data中渲染到页面
-
-      carts: carts,
-
-      totalPrice: total.toFixed(2)
-
-    });
-
+    console.log(arr)
+    that.setData({
+      carts: that.data.carts,
+      batchIds: arr,
+      selectAllStatus: selectAllStatus
+    })
   },
   selectList(e) {
-
+    console.log(e)
     const index = e.currentTarget.dataset.index; // 获取data- 传进来的index
-
     let carts = this.data.carts; // 获取购物车列表
-
     const selected = carts[index].selected; // 获取当前商品的选中状态
-
     carts[index].selected = !selected; // 改变状态
-
     this.setData({
-
       carts: carts
-
     });
-
-    this.getTotalPrice(); // 重新获取总价
 
   },
-  selectAll(e) {
-    let selectAllStatus = this.data.selectAllStatus; // 是否全选状态
-    selectAllStatus = !selectAllStatus;
-    let carts = this.data.carts;
-    for (let i = 0; i < carts.length; i++) {
-      carts[i].selected = selectAllStatus; // 改变所有商品状态
-    }
-    this.setData({
-      selectAllStatus: selectAllStatus,
-      carts: carts
-    });
-    this.getTotalPrice(); // 重新获取总价
+
+  confirmbtn: function() {
+    console.log(this.data.batchIds)
   },
   /**
    * 生命周期函数--监听页面隐藏
