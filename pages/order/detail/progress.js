835711b2d6c4f4d5df937532ba8e8439e84b4738
郝艳萍 <2860435000@qq.com>
2019-11-06 17:05:15 +0800

    退款流程

--- pages/order/detail/progress.js
+++ pages/order/detail/progress.js
@@ -10,63 +10,94 @@ Page({
     showIcon: true,
     gloheight: t.globalData.gloheight,
     globalimg: t.globalData.appimg,
+    expresslist: [{
+        time: '2018-2-15',
+        step: '退款成功,商家同意退款'
+      },
+      {
+        time: '2018-3-15',
+        step: '商家已同意你的退款申请,请耐心等耐'
+      },
+      {
+        time: '2018-4-15',
+        step: '你的退款申请已提交'
+      }
+
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       show: !0,
     })
   },
+  onLoad: function(s) {
+    console.log(s)
+    var s = {
+      id: "40803"
+    }
+    this.setData({
+      show: !0,
+      options: s
+    })
+  },
+  // get_list: function () {
+  //   var t = this;
+  //   a.get("order/express", t.data.options, function (e) {
+  //     console.log(e)
+  //     0 == e.error ? (e.show = !0, t.setData(e)) : a.toast(e.message, "loading");
+  //   });
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
