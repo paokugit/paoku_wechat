26f9628946347e680e7060bf4da5f2326182c4e2
郝艳萍 <2860435000@qq.com>
2019-11-14 11:56:08 +0800

    他的店页面

---
+++ packageA/pages/hestore/hestore/hestore.js
@@ -0,0 +1,91 @@
+// pages/order/detail/progress.js
+var t = getApp(),
+  a = t.requirejs("core");
+var f = getApp();
+var useropenid = ''
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    showIcon: true,
+    gloheight: t.globalData.gloheight,
+    globalimg: t.globalData.appimg,
+    infolist: [],
+    page: 1,
+    type: 0,
+    loaded: !1,
+    loading: !0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var userinfo = f.getCache('userinfo');
+    useropenid = userinfo.openid
+    this.setData({
+      show: !0,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+  myTab: function (t) {
+    console.log(t)
+    var e = this, i = a.pdata(t).type;
+    e.setData({
+      type: i,
+      page: 1,
+      list: [],
+      loading: !0
+    });
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
