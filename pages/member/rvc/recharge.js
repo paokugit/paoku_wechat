b1d9a7ef964c2907af94efad265cdc4fdb4a8575
xph <2595222190@qq.com>
2019-11-16 17:14:52 +0800

    RVC页面功能实现

---
+++ pages/member/rvc/recharge.js
@@ -0,0 +1,106 @@
+var t = getApp(),
+  a = t.requirejs("core");
+var f = getApp();
+
+var useropenid = "";
+var str = 0;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    globalimg: t.globalData.appimg,
+    showIcon: true,
+    gloheight: t.globalData.gloheight,
+
+    loading: !0,
+ 
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var userinfo = f.getCache('userinfo');
+    useropenid = userinfo.openid;
+
+    this.setData({
+      show: !0,
+      loading: !1,
+      record:-1
+    })
+  },
+
+  formName: function (e) {
+    var m = this;
+    str = e.detail.value;
+    var num = str.indexOf('.');
+   
+    if(num != -1){
+      var long = str.length;
+      var errand = long-num-1;
+      if (errand >=6){
+        m.setData({
+          record: long
+        })
+      }
+    }
+  },
+
+  paybtn:function(e){
+    console.log(str);
+    wx.navigateTo({
+      url: '/pages/member/webview/webview',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/member/rvc/rvc.js
@@ -0,0 +1,89 @@
+var t = getApp(),
+  a = t.requirejs("core");
+var f = getApp();
+
+var useropenid = "";
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: { 
+    globalimg: t.globalData.appimg,
+    showIcon: true,
+    gloheight: t.globalData.gloheight,
+
+    credit: '',
+    come_total: '',
+    frozen_credit: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var userinfo = f.getCache('userinfo');
+    useropenid = userinfo.openid;
+
+    var t = this
+    a.get('member/log/member_RVC', {
+      openid: userinfo.openid
+    }, function (e) {
+      console.log(e)
+      t.setData({
+        credit: e.info.RVC,
+        come_total: e.info.come_total,
+        frozen_credit: e.info.sale_total
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
