498496651f058d5b1ce0fb18ff5634e62c35d083
xph <2595222190@qq.com>
2019-11-20 20:50:09 +0800

    rvc测试

--- pages/member/rvc/recharge.js
+++ pages/member/rvc/recharge.js
@@ -5,6 +5,8 @@ var f = getApp();
 
 var useropenid = "";
 var str = 0;
+
+var uuid;
 Page({
 
   /**
@@ -61,9 +63,36 @@ Page({
       }, function (e) {
         console.log(e);
         if(e.error == 0){
-          wx.navigateTo({
-            url: '/pages/member/webview/webview?str='+e.data.payUrl,
+          uuid = e.data.uuid;
+          wx.showModal({
+            title: '请点击确定按钮复制链接，在浏览器中打开完成支付',
+            content: e.data.payUrl,
+            success: function (res) {
+              if (res.confirm) {
+                
+                wx.setClipboardData({
+                  data: e.data.payUrl,
+                  success: function (res) {
+                    wx.getClipboardData({
+                      success: function (res) {
+                        wx.showToast({
+                          title: '复制成功'
+                        })
+                      }
+                    })
+                  }
+                })
+
+              } else {
+                console.log('用户点击取消')
+              }
+
+            }
+
           })
+          // wx.navigateTo({
+          //   url: '/pages/member/webview/webview?str='+e.data.payUrl,
+          // })
         }else if(e.error == 1){
           wx.showToast({
             title: e.message,
@@ -87,7 +116,33 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
- 
+    console.log(uuid);
+    if (uuid != undefined){
+      a.get("game.check_rvc",{
+        uuid: uuid
+      },function(e){
+        console.log(e);
+        
+        wx.showModal({
+          title: '提示',
+          content: e.message,
+          success: function (res) {
+            if (res.confirm) {
+              console.log('用户点击确定')
+              wx.navigateBack({
+                delta: 1
+              })
+            } else {
+              console.log('用户点击取消')
+              wx.navigateBack({
+                delta: 1
+              })
+            }
+          }
+        })
+        
+      })
+    }
   },
 
   /**
