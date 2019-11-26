4f69829c224df0c60ea912f9242327832d128ff8
郝艳萍 <2860435000@qq.com>
2019-11-26 18:34:44 +0800

    三级联动

--- pages/jdproducts/address/add.js
+++ pages/jdproducts/address/add.js
@@ -1,22 +1,33 @@
-var a, e, i = getApp(),
-  s = i.requirejs("core");
-var f = getApp();
-//定义三列数组数据，用于存储省、市、县
-let multiArray0 = [];
-let multiArray1 = [];
-let multiArray2 = [];
-const app = getApp();
-
+var t = getApp(),
+  e = t.requirejs("core"),
+  a = t.requirejs("foxui"),
+  i = t.requirejs("jquery");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    globalimg: i.globalData.appimg,
+    globalimg: t.globalData.appimg,
     showIcon: true,
-    region: ['广东省', '广州市', '海珠区'],
-    multiIndex: [],
+    subtext: "保存地址",
+    province_list: null,
+    province_name: null,
+    city_list: null,
+    city_name: null,
+    area_list: null,
+    area_name: null,
+    addressCity: null,
+    multiArray: [], // 三维数组数据
+    multiIndex: [0, 0, 0], // 默认的下标,
+    selectProvinceId: null,
+    selectCityId: null,
+    selectAreaId: null,
+    realname: "",
+    mobile: "",
+    areas: "",
+    street: "",
+    address: ""
 
   },
 
@@ -53,170 +64,186 @@ Page({
       show: !0
     })
     var that = this;
+    that.getProvince()
+  },
+
+  //获取省份列表
+  getProvince: function() {
+    let that = this
     wx.request({
       url: 'http://192.168.3.102/app/ewei_shopv2_api.php?i=1&r=app.superior.address&comefrom=wxapp',
       data: {
         type: 0,
         id: 0
       },
-      method: "POST",
       header: {
         'content-type': 'application/json' // 默认值
       },
-      success: function(res) {
-        console.log(res);
-        var xiaoquList = res.data.data.province;
-        console.log(xiaoquList)
-        var xiaoquArr = xiaoquList.map(item => {　　　　 // 此方法将省名称区分到一个新数组中
-          // console.log(item.name)
-          return item.name;
-        });
-        console.log(xiaoquArr)
+      success(res) {
+        console.log(res)
+        let provinceList = [...res.data.data.province] //放到一个数组里面
+        let provinceArr = res.data.data.province.map((item) => {
+          return item.name
+        }) //获取名称
         that.setData({
-          multiArray: [xiaoquArr, []],
-          xiaoquList,
-          xiaoquArr
+          multiArray: [provinceArr, [],
+            []
+          ], //更新三维数组  更新完为[['广东','北京'],[],[]]
+          province_list: provinceList, //省级原始数据
+          province_name: provinceArr, //省级全部名称
+
         })
-        var default_xiaoqu_id = xiaoquList[0]['id'];　　　　 //获取默认的省对应的id
-        if (default_xiaoqu_id) {
-          console.log(default_xiaoqu_id)
-          that.searchClassInfo(default_xiaoqu_id)　　　　　　 // 如果存在调用获取对应的省级数据
+        console.log(that.data.multiArray)
+        let defaultCode = that.data.province_list[0].id //使用第一项当作参数获取市级数据
+        if (defaultCode) {
+          that.setData({
+            currnetProvinceId: defaultCode //保存当前省份id
+          })
+          console.log(defaultCode)
+          that.getCity(defaultCode) //获取市区数据
         }
       }
     })
   },
-  //市级
-  searchClassInfo(xiaoqu_id) {
-    var that = this;
-    if (xiaoqu_id) {
-      this.setData({
-        pid: xiaoqu_id
-      }, () => {
-        wx.request({
-          url: 'http://192.168.3.102/app/ewei_shopv2_api.php?i=1&r=app.superior.address&comefrom=wxapp',
-          https: '',
-          method: "POST",
-          header: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json'
-          },
-          data: {
-            type:1,
-            id: that.data.pid
-          },
-          success: function(res) {
-            console.log(res);
-            var classList = res.data.data.province;
-            var classArr = classList.map(item => {
-              // console.log(item.name)
-              return item.name;
-            })
-            var xiaoquArr = that.data.xiaoquArr;
-            that.setData({
-              multiArray: [xiaoquArr, classArr],
-              classArr,
-              classList
-            })
-            var default_xiaoqu_id = classList[0]['id'];　　　　 //获取默认的市对应的id
-            if (default_xiaoqu_id) {
-              console.log(default_xiaoqu_id)
-              that.searchRegionInfo(default_xiaoqu_id)　　　　　　 // 如果存在调用获取对应的省级数据
-            }
-          }
+  //根据省份id获取城市
+  getCity: function(id) {
+    let that = this
+    that.setData({
+      currnetProvinceId: id
+    })
+    // console.log("市", id)
+    wx.request({
+      url: 'http://192.168.3.102/app/ewei_shopv2_api.php?i=1&r=app.superior.address&comefrom=wxapp',
+      data: {
+        type: 1,
+        id: id
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res)
+        // console.log(res.data.data)
+        let cityArr = res.data.data.province.map((item) => {
+          return item.name
+        }) //返回城市名称
+        let cityList = [...res.data.data.province]
+        console.log(cityList)
+        that.setData({
+          multiArray: [that.data.province_name, cityArr, []], //更新后[['广东','北京'],['潮州'，'汕头','揭阳'],[]]
+          city_list: cityList, //保持市级数据
+          city_name: cityArr //市级名称
         })
-      })
-    }
-  },
-  //区
-  searchRegionInfo(xiaoqu_id) {
-    var that = this;
-    if (xiaoqu_id) {
-      this.setData({
-        pid: xiaoqu_id
-      }, () => {
-        wx.request({
-          url: 'http://192.168.3.102/app/ewei_shopv2_api.php?i=1&r=app.superior.address&comefrom=wxapp',
-          https: '',
-          method: "POST",
-          header: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json'
-          },
-          data: {
-            type: 2,
-            id: that.data.pid
-          },
-          success: function (res) {
-            console.log(res);
-            var regionList = res.data.data.province;
-            var regionArr = regionList.map(item => {
-              // console.log(item.name)
-              return item.name;
-            })
-            var xiaoquArr = that.data.xiaoquArr;
-            var classArr = that.data.classArr
-            that.setData({
-              multiArray: [xiaoquArr, classArr,regionArr],
-              regionArr,
-              regionList,
-            })
-          }
-        })
-      })
-    }
-  },
-  bindMultiPickerColumnChange: function(e) {
-    //e.detail.column 改变的数组下标列, e.detail.value 改变对应列的值
-    console.log(e);
-    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
-    var data = {
-      multiArray: this.data.multiArray,
-      multiIndex: this.data.multiIndex
-    };
-
-    data.multiIndex[e.detail.column] = e.detail.value;
-    var pid_session = this.data.pid;　　　　 // 保持之前的省id 与新选择的id 做对比，如果改变则重新请求数据
-    switch (e.detail.column) {
-      case 0:
-        var xiaoquList = this.data.xiaoquList;
-        var pid = xiaoquList[e.detail.value]['id'];
-        if (pid_session != pid) {　　　　 // 与之前保持的省id做对比，如果不一致则重新请求并赋新值
-          this.searchClassInfo(pid);
+        console.log(that.data.multiArray)
+        let defaultCode = that.data.city_list[0].id //获取第一个市的区级数据
+        if (defaultCode) {
+          that.setData({
+            currentCityId: defaultCode //保存当下市id
+          })
+          that.getArea(defaultCode) //获取区域数据
         }
-        data.multiIndex[1] = 0;
-        break;
-    }
-    this.setData(data);
+      }
+    })
   },
-  bindMultiPickerChange: function(e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    var class_key = 0;
-    var classList = this.data.classList;
-    var select_key = e.detail.value[1];
-    var real_key = select_key - 1;
-    if (real_key < class_key) {
+  //获取区域
+  getArea: function(id) {
+    let that = this
+    that.setData({
+      currentCityId: id //保存当前选择市
+    })
+    wx.request({
+      url: 'http://192.168.3.102/app/ewei_shopv2_api.php?i=1&r=app.superior.address&comefrom=wxapp',
+      data: {
+        type: 2,
+        id: id
+      },
+      success(res) {
+        // console.log(res.data.data)
+        let areaList = [...res.data.data.province]
+        let areaArr = res.data.data.province.map((item) => {
+          return item.name
+        }) //区域名
+        that.setData({
+          multiArray: [that.data.province_name, that.data.city_name, areaArr],
+          area_list: areaList, //区列表
+          area_name: areaArr //区名字
+        })
+
+      }
+    })
+  },
+  //picker确认选择地区
+  bindRegionChange: function(e) {
+    // 因为在获取省中 北京只有一个选项，导致获取不了北京》北京》第一个
+    if (e.detail.value[1] == null || e.detail.value[2] == null) { //如果只滚动了第一列则选取第一列的第一数据
       this.setData({
-        cid: 0
+        multiIndex: e.detail.value, //更新下标
+        addressCity: [this.data.province_list[e.detail.value[0]].name, this.data.city_list[0].name, this.data.area_list[0].name],
+        selectProvinceId: this.data.province_list[e.detail.value[0]].id,
+        selectCityId: this.data.city_list[0].id,
+        selectAreaId: this.data.area_list[0].id
       })
     } else {
       this.setData({
-        cid: classList[real_key]['cid']　　　　　　 // class_id 代表着选择的市级对应的 班级id
+        multiIndex: e.detail.value, //更新下标
+        addressCity: [this.data.province_list[e.detail.value[0]].name, this.data.city_list[e.detail.value[1]].name, this.data.area_list[e.detail.value[2]].name],
+        selectProvinceId: this.data.province_list[e.detail.value[0]].id,
+        selectCityId: this.data.city_list[e.detail.value[1]].id,
+        selectAreaId: this.data.area_list[e.detail.value[2]].id
       })
     }
-    this.setData({
-      multiIndex: e.detail.value
-    })
+    console.log(this.data.selectProvinceId, this.data.selectCityId, this.data.selectAreaId)
+  },
+  //滑动地区组件
+  bindRegionColumnChange: function(e) {
+    // console.log(e.detail.column,e.detail.value)
+    let that = this
+    let column = e.detail.column //当前改变的列
+    let data = {
+      multiIndex: JSON.parse(JSON.stringify(that.data.multiIndex)),
+      multiArray: JSON.parse(JSON.stringify(that.data.multiArray))
+    }
+    data.multiIndex[column] = e.detail.value //第几列改变了就是对应multiIndex的第几个，更新他
+    switch (column) {
+      case 0: //第一列改变，省级改变
+        let currentProvinceId = that.data.province_list[e.detail.value].id
+        if (currentProvinceId != that.data.currentProvinceId) { //判断当前id是不是更新了
+          that.getCity(currentProvinceId) //获取当前id下的市级数据
+        }
+        data.multiIndex[1] = 0 //将市默认选择第一个
+        break
+      case 1: //第二列改变，市级改变
+        let currentCityId = that.data.city_list[e.detail.value].id
+        if (currentCityId != that.data.currentCityId) {
+          that.getArea(currentCityId) //获取区域
+        }
+        data.multiIndex[2] = 0 //区域默认第一个
+        break
+    }
+    that.setData(data) //更新数据
   },
 
-
-  bindRegionChange: function(e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+  nameChange: function(t) {
+    console.log(t)
     this.setData({
-      region: e.detail.value
+      realname: t.detail.value,
     })
   },
-
-
+  phoneChange: function(t) {
+    console.log(t)
+    this.setData({
+      mobile: t.detail.value,
+    })
+  },
+  streetChange: function(t) {
+    console.log(t)
+    this.setData({
+      address: t.detail.value,
+    })
+  },
+  submit: function() {
+    console.log(this.data.realname, this.data.mobile)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
